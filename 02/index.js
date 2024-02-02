528cf67ccb2c253c5db7dbce976e2e19e67175b6
Adams Lech <adams.a.lech@hotmail.com>
2024-02-01 22:29:44 -0300

    up to exercicios 2 e 3

--- 02/index.js
+++ 02/index.js
@@ -1,1 +1,23 @@
+//Exercício 02
+
+// Contando letras
+
+//Declare uma nova variável que contem um array contendo algumas letras.
+
+//Faça um programa que conta quantas letras "E" ou "e" existem nesse array.
+
+//Imprima mensagens bonitinhas para mostrar o resultado, por favor. Inclusive quando nenhuma letra for encontrada.
+
 const letras = ["A", "a", "B", "C", "E", "e"];
+
+let encontrado = false;
+
+for (let letra of letras) {
+    if (letra === 'E' || letra === 'e') {
+        console.log(`A letra ${letra} foi encontrada`);
+        encontrado = true;
+    }
+}
+if (!encontrado) {
+    console.log(`A letra não foi encontrada`);
+}
--- 03/index.js
+++ 03/index.js
@@ -1,2 +1,10 @@
-const numeros = [54, 22, 14, 87, 10, 284];
+const numeros = [54, 22, 14, 10, 87, 284];
+
+
+let indice = 0;
+
+while (indice < numeros.length) {
+    console.log(numeros.indexOf(10));
+    break;
+}
 
