5e95811362e748bcee5931bf13501ea2d210219a
Adams Lech <adams.a.lech@hotmail.com>
2024-02-14 23:12:01 -0300

    exercicio 04

---
+++ 04/index.js
@@ -0,0 +1,14 @@
+/*Exercício 04
+
+## Múltiplos de 3
+
+Faça um programa que imprima na tela todos os múltiplos de 3 compreendidos entre 0 e 500.
+
+Os múltiplos de 3 são: 0, 3, 6, 9, 12... */
+
+let numeros = 0;
+
+while (numeros <= 500) {
+    console.log(numeros);
+    numeros += 3;
+}
