06afb98da281cbca6f988a7ec93574714b7a6028
Adams Lech <adams.a.lech@hotmail.com>
2024-02-15 22:20:42 -0300

    exercicio 06

--- 06/index.js
+++ 06/index.js
@@ -1,2 +1,19 @@
+/*Exercício 06
+
+## Soma dos pares
+
+Declare um array com alguns números inteiros quaisquer.
+
+Depois, percorra este array e calcule a **soma dos números pares** 
+presentes nesse array.
+*/
+
 const numeros = [3, 4, 7, 8, 1, 6, 5, 10];
 
+let somaPares = 0;
+
+for (let pares of numeros) {
+    if (pares % 2 === 0) {
+        somaPares += pares;
+    }
+} console.log(somaPares);
