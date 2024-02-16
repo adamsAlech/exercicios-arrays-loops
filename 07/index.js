c698cfbe2c98550b4c88e9eae7d5666e25271a4d
Adams Lech <adams.a.lech@hotmail.com>
2024-02-15 23:15:54 -0300

    exercicio 07

--- 07/index.js
+++ 07/index.js
@@ -1,1 +1,29 @@
+/*Exercício 07
+
+## Filtrando nomes com a letra A
+
+Declare um array com alguns nomes quaisquer.
+
+Depois, crie um novo array a partir do primeiro que 
+contenha apenas os nomes que começam com a letra "A" 
+ou "a"(maiúscula ou minúscula).
+Ao final, imprima a variável que guarda o array.
+*/
+
 const nomes = ["Ana", "Joana", "Carlos", "amanda"];
+let encontrado = false;
+for (let nomesComA of nomes) {
+    for (let i = 0; i < nomesComA.length; i++) {
+        if (nomesComA[0] === 'A') {
+            console.log(nomesComA);
+            encontrado = true;
+            break
+        } else if (nomesComA[0] === 'a') {
+            console.log(nomesComA);
+            encontrado = true;
+            break
+        }
+    }
+} if (!encontrado === true) {
+    console.log('Aão á nomes com a letra A');
+}
