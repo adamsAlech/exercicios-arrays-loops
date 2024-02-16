fc96131e043882fd87d74c4da6c4bea7189fc579
Adams Lech <adams.a.lech@hotmail.com>
2024-02-15 23:58:48 -0300

    exercicio 10 comecado

---
+++ 10/index.js
@@ -0,0 +1,11 @@
+/*Exercício 10
+
+## Para pensar um pouco mais
+
+Declare um array com alguns números inteiros quaisquer.
+
+Depois, faça um programa que calcule a maior diferença 
+entre dois números desse array.
+*/
+
+const numeros = [8, 11, 4, 1];
