70527b7b4b26ab4dacc4814c864ad8cd15af0934
Adams Lech <adams.a.lech@hotmail.com>
2024-02-15 23:38:45 -0300

    exercicio 08

--- 08/index.js
+++ 08/index.js
@@ -1,1 +1,15 @@
+/*Encontrando o maior
+
+Declare um array com alguns números quaisquer.
+
+Depois, faça um programa que encontre o maior número 
+desse array e o imprima no console.
+*/
+
 const numeros = [3, 24, 1, 8, 11, 7, 15];
+let maior = 0;
+for (let item of numeros) {
+    if (item > maior) {
+        maior = item;
+    }
+} console.log(maior);
