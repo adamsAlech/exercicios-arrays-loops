26e74c13deeacc39eaad8b99d2f2f6f3354a8547
Adams Lech <adams.a.lech@hotmail.com>
2024-02-18 22:09:34 -0300

    exercicio 13

--- 13/index.js
+++ 13/index.js
@@ -1,2 +1,21 @@
+/*Exercício 13
+
+## Verificando disjuntores
+
+Precisamos verificar em uma caixa de disjuntores quais estão ligados.
+
+A caixa de disjuntores é representada por um array onde, cada item do 
+array é um disjuntor. Nesse array o índice do item é a identificação do 
+disjuntor (começando em 0) e o valor do item, um `boolean`, é a indicação 
+se o disjuntor está ligado ou não. Imprima no console a posição **apenas 
+dos disjuntores que estão ligados**, se possível, utilizar o conceito de **_truthiness_**.
+
+*/
+
 const disjuntores = [false, false, true, false, false, true, false, false];
 
+for (let ligado of disjuntores) {
+    if (!ligado === false) {
+        console.log(ligado);
+    }
+}
